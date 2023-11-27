31928a5c65c20c57b932b1f5b751edbcbd7a94e5
aapdo <jade04342@gmail.com>
2023-11-27 23:34:27 +0900

    sql insert hospital

--- init.sql
+++ init.sql
@@ -39,3 +39,21 @@ CREATE TABLE `likeTbl` (
 ALTER TABLE `likeTbl`
 ADD CONSTRAINT `FK_User_TO_like_1` FOREIGN KEY (`userId`) REFERENCES `User` (`userId`),
 ADD CONSTRAINT `FK_Hospital_TO_like_1` FOREIGN KEY (`hospitalId`) REFERENCES `Hospital` (`hospitalId`);
+
+INSERT INTO hospital (tel, address, hosName, hosPic) 
+VALUES ('02-953-0075', '서울 성북구 동소문로 73 시티플레이스 B1, 1F, 2F', 'VIP동물의료센터', 'maltese.jpg');
+
+INSERT INTO hospital (tel, address, hosName, hosPic) 
+VALUES ('02-6956-0040', '서울 성북구 돈암동 644-2', '원러브동물의료센터', 'poodle.jpg');
+
+INSERT INTO hospital (tel, address, hosName, hosPic) 
+VALUES ('02-929-5496', '서울 성북구 동소문로 66 김의원빌딩', '나래동물병원', 'shiba.jpg');
+
+INSERT INTO hospital (tel, address, hosName, hosPic) 
+VALUES ('02-927-7878', '서울 성북구 아리랑로 27 1층, 2층', '로이동물병원', 'maltese.jpg');
+
+INSERT INTO hospital (tel, address, hosName, hosPic) 
+VALUES ('02-529-5575', '서울 강남구 도곡로 189 1층', '예은동물병원', 'poodle.jpg');
+
+INSERT INTO hospital (tel, address, hosName, hosPic) 
+VALUES ('02-574-7533', '서울 강남구 선릉로 806 킹콩빌딩 5층', '이안동물의학센터', 'shiba.jpg');
