cec55241c2851a9fb4d845e4c0a97e7110b9885d
aapdo <jade04342@gmail.com>
2023-11-29 15:34:12 +0900

    test

--- src/main/java/like/service/LikeService.java
+++ src/main/java/like/service/LikeService.java
@@ -7,9 +7,11 @@ import like.entity.Like;
 
 public class LikeService {
 	private final LikeDao likeDao = new LikeDao();
+
 	public List<Like> getLikesByHospitalId(Long hosId){
 		return likeDao.getLikesByHospitalId(hosId);
 	}
+
 	public List<Like> getLikesByUserId(Long userId){
 		return likeDao.getLikesByUserId(userId);
 	}
